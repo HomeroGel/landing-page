4dd8c27bd80b4ecc09298f61d3d9af1d783e4c10
Homero Gelves <homerogelves@gmail.com>
2023-05-25 15:45:46 -0300

    sigo probando cosas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,22 @@
-import images from '../images.json'
+// import images from '../images.json'
 
-const data = images.images
+// const data = images.images
 
 function Cards() {
     return (
         
             <div className='container mx-auto mt-20'>
                 <div className='flex flex-wrap gap-20 justify-center'>
-                    {data.map((image, index) => (
+                <div className='relative'>
+                            <img src='./src/assets/desktop/imgEarth.jpg' alt="" className='brightness-50'/>
+                            <p className='absolute top-[22rem] text-2xl w-52 px-11 text-slate-300'>Hola</p>
+                        </div>
+                    {/* {data.map((image, index) => (
                         <div className='relative' key={index}>
                             <img src={image.image} alt="" className='brightness-50'/>
                             <p className='absolute top-[22rem] text-2xl w-52 px-11 text-slate-300'>{image.name}</p>
                         </div>
-                    ))}
+                    ))} */}
                 </div>
             </div>
         
